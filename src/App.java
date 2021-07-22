5d04e0e569d0d6f412610f372d87d846a1c61dcf
hqpiotr <eu.piotr@gmail.com>
2021-07-22 20:45:58 +0200

    update

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
+import java.util.TimeZone;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println("And the time is...: " + System.currentTimeMillis() );
     }
 }
 
